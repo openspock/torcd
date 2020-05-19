c97fb0ca5baefba8773b50a2bb9d51e7986c3893
Ameya Bhurke <ameya@waddle.com.au>
2020-05-19 14:32:16 +1000

    add config to read proc config

--- include/config.hpp
+++ include/config.hpp
@@ -1,6 +1,8 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <optional>
+#include <filesystem>
 #include <string>
 #include <vector>
 #include <cinttypes>
@@ -13,12 +15,22 @@ namespace torc {
             std::string     cf_name; // name entry
             std::uint32_t   cf_t_cnt; // count of parallel executions
             std::string     cf_cmd; // command to be executed
-        }
+        };
 
         struct Base
         {
             std::vector<Proc> procs;
-        }
+        };
+
+        // factory function to create a base config
+        std::optional<Base> create();
+
+        // returns the home directory of torc -
+        //
+        // if TORC_HOME env var is set, then return that, or
+        // else return current dir
+        std::filesystem::path home_dir();
+    }
 }
 
 #endif
